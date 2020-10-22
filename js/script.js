61b09cbd2c0d49ab8288bd7e247d25e1c1c6ac7d
francesca-giorno <francescagiorno1996@gmail.com>
2020-10-22 15:58:25 +0200

    email

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*email*/
+
+var email = ["francesca96@gmail.com", "francescagiorno@hotmail.com", "francescagiorno1996@gmail.com", "francesca.giorno@libero.it", "fragiorno@virgilio.com"];
+var emails = prompt("qual è la tua email?");
+var mex = prompt(" Risultato non trovato!");
+
+for (var i= 0; i < email.legnth; i++);
+
+ if(email[i] = emails){
+   mex ="Risultato non trovato";
+ }
+
+console.log(mex);
